6d712caea914f0b51c53a2120fbcd7a44b193bce
ripinsurreality <ripinsurreality@gmail.com>
2021-07-01 11:57:33 +0300

    add bloggers page, blogger module, blogger image assets

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,14 +1,18 @@
 import React from "react"
 import Nav from "@modules/nav/Nav"
 import Footer from "@modules/footer/Footer"
+import Bloggers from "./bloggers/Bloggers"
 
 interface MainProps {}
 
 const Main: React.FC<MainProps> = () => {
-    return (<div>
-        <Nav breadcrumbs={["Главная", "Блогеры"]}/>
-        <Footer />
-    </div>)
+  return (
+    <div>
+      <Nav breadcrumbs={["Главная", "Блогеры"]} />
+        <Bloggers />
+      <Footer />
+    </div>
+  )
 }
 
 export default Main
